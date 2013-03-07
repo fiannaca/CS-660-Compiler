7e4b2b7c75339e59dacf56fa67b55600eab85f14
Alex Fiannaca <fiannac4@live.com>
2013-03-06 23:00:44 -0800

    Setup the main driver to require a filename as an argument. It then redirects the stdin stream to the file and begins parsing. Currently there are two known issues which must be fixed: 1) Multiline style comments must be dealt with, 2) hexidecimal float values are not being tokenized correctly yet.

--- main.cc
+++ main.cc
@@ -1,7 +1,19 @@
+#include <stdio.h>
+
 #include "Parser.h"
 
 int main(int argc, char **argv)
 {
+    if(argc > 1)
+    {
+        freopen(argv[1], "r", stdin);
+    }
+    else
+    {
+        printf("\nUsage: ./CFrontEnd [filename.c]\n\n");
+        return 1;
+    }
+
     Parser parser;
 
     parser.parse();
