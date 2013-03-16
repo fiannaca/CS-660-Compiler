6e7627d3a0e1e1961dd52cf6832702929a99f2b3
Thomas Madappattu <sandeep.sandos@gmail.com>
2013-03-16 09:34:05 -0700

    Stub symbol table so that u can proceed
    each symbol has a entry in the TypeTable vector , this makes in easy to
    incorporate typedefs. The  Type is a list , so int ** , would be  <int>
    <star> <star>  . TypeTable vector nees to be populated initially.

---
+++ flexbison/SymTab.h
@@ -0,0 +1,60 @@
+#ifndef _SYMTAB_H_
+#define _SYMTAB_H_
+#include <string>
+#include <map>
+#include <list>
+#include <vector>
+using namespace std;
+
+enum STORAGE_CLASS
+{
+   EXTERN ,
+   STATIC ,
+   REGISTER ,
+   AUTO	
+
+};
+
+enum  BASIC_TYPES
+{
+   INT,
+   CHAR ,
+   FLOAT,
+   DOUBLE,
+   STRUCT,
+   UNION	
+
+};
+
+struct TypeInfo
+{
+    list<int> typeList; 	
+};
+
+struct SymbolInfo
+{
+    string symbol_name;
+    bool struct_union_name;
+    bool isStrunctOrUnionItem; 
+	int typeTableIndex;           
+    int storage_class;
+    int flags;
+		
+};
+
+vector<TypeInfo> typeTable;
+class SymTab
+{
+     
+	 private:
+	     
+	     list< map<string,SymbolInfo> >  symTable;
+	 public:	
+	     void insert_symbol(SymbolInfo symbolInfo);
+	     void insert_symbol(SymbolInfo symbolInfo, int level);
+	     int search_symbol(SymbolInfo &symbolInfo,int &level);
+   	     void dump_table();
+         void dump_table(int level);  
+};
+
+#endif 
