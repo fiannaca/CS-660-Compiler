93dcd4d5c6de82179c2d2d5133e141e2c878108a
Alex Fiannaca <fiannac4@live.com>
2013-03-29 09:52:03 -0700

    Added DOT output class and instructions for how to create the output graph

--- flexbison/ast/Ast.h
+++ flexbison/ast/Ast.h
@@ -1,11 +1,36 @@
 #ifndef _AST_H_
 #define _AST_H_ 
+
+#include <string>
+#include "Visualizer.h"
+
 class AST
 {
-public:
-	virtual void Visit(); 
-	
+    public:
+        AST()
+        {
+            uid = Visualizer::GetNextUID();
+            label = "LabelNotSet";
+        }
+
+        void setLabel(string l)
+        {
+            label = l;
+        }
+
+        int getUID() { return uid; }
+
+        string getLabel() { return label; }
+
+        static Visualizer vis;
+
+	virtual void Visit(){};
+
+    protected:
+        int uid;
+        string label;
 };
 
+Visualizer AST::vis;
 
 #endif 
--- flexbison/ast/cs660.cpp
+++ flexbison/ast/cs660.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <string>
 #include "Ast.h"
@@ -18,10 +16,31 @@
 #include "AstStatementList.h"
 #include "AstUnaryOperator.h"
 #include "AstBinaryOperator.h" 
+
 using namespace std;
 
 int main()
 {
+    AstUnary anode(NULL, "blah");
+    AstUnary anode2(NULL, "blah");
+    AstUnary anode3(NULL, "blah");
 
+    anode.setLabel("UnaryOperator1");
+    anode2.setLabel("UnaryOperator2");
+    anode3.setLabel("UnaryOperator3");
 
+    //Build a test tree out of order to verify that it still works
+
+    //Child 1
+    AST::vis.addNode(anode2.getUID(), anode2.getLabel());
+
+    //Parent
+    AST::vis.addNode(anode.getUID(), anode.getLabel());
+    AST::vis.addEdge(anode.getUID(), anode2.getUID());
+
+    //Child 2
+    AST::vis.addNode(anode3.getUID(), anode3.getLabel());
+    AST::vis.addEdge(anode.getUID(), anode3.getUID());
+
+    return 0;
 }
